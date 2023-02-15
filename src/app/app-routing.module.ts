7501f619e7c094178210416c612cab2d9e084c28
Valentino Alejandro Terreno <ninot2016@gmail.com>
2023-02-14 21:09:26 -0300

    Componente Banner - Componente Home
    Se creo componente Home como pagina de inicio o principal.
    Se creo componente Banner donde se muestra una imagen de la institución con el propio nombre.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 const routes: Routes = [
-  {path:'', component: HeaderComponent}];
+  {path:'', component: HomeComponent},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BannerComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
